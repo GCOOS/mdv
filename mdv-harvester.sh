ff94013e4aa759c19b25db946abf40ee51cb25cd
Bob Currier <bob.currier@gcoos.org>
2016-07-07 13:54:25 +0000

    added mdv-harvester.sh script

---
+++ mdv-harvester.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rsync -av rdc@gcoos4.tamu.edu:/data/nrl_rpsea/2016*/ncom_relo_mean* /data/mdv/nrl_relo_mean/
+rsync -av rdc@gcoos4.tamu.edu:/data/nrl_rpsea/2016*/ncom_relo_std* /data/mdv/nrl_relo_std/
