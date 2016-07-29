8b0a30fc9b202aa852ffce27348a088ed408094b
Bob Currier <robertdcurrier@gmail.com>
2016-07-29 15:21:38 -0400

    Added Global HYCOM

--- mdv-harvester.sh
+++ mdv-harvester.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rsync -av rdc@gcoos4.tamu.edu:/data/nrl_rpsea/2016*/ncom_relo_mean* /data/mdv/nrl_relo_mean/
 rsync -av rdc@gcoos4.tamu.edu:/data/nrl_rpsea/2016*/ncom_relo_std* /data/mdv/nrl_relo_std/
+rsync -avr rdc@gcoos4.tamu.edu:/data/ndbc_dods/dods_data /data/mdv/ndbc_dods
